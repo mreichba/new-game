cab2fb1de50da420d3c3d82af3f2fab08c238a7b
Mason Reichbauer <mreichba@mail.usf.edu>
2020-08-10 15:02:01 -0400

    added scenes

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
-const { Phaser } = require("./phaser.min")
+let config = {
+  width: 1200,
+  height: 800,
+  backgroundColor: 0x000000,
+  scene: [Scene1, Scene2]
+};
 
 window.onload = function () {
-  let game = new Phaser.Game();
+  let game = new Phaser.Game(config);
 };
